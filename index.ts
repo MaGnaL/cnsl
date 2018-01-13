33c9d3b1e2f8a26237c0710225138bfe9e53da0d
Manuel <code@magnal.io>
2018-01-13 12:47:47 +0100

    chore: update versions and project setup (dist folder)

--- index.ts
+++
@@ -1,2 +0,0 @@
-export {Cnsl, cnsl} from './src/cnsl.class';
-export {cnslLogged, CnslLoggedDecoratorConfig} from './src/cnsl-logged.decorator';
---
+++ lib/index.ts
@@ -0,0 +1,2 @@
+export {Cnsl, cnsl} from './cnsl.class';
+export {cnslLogged, CnslLoggedDecoratorConfig} from './cnsl-logged.decorator';
